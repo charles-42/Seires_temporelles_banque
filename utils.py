d6752f7cef5cfbdae08ca6fcaa4f71d4c780f587
charles-42 <71760090+charles-42@users.noreply.github.com>
2023-04-25 13:39:02 +0200

    feat: :sparkles: connect, create table, import data and request data

---
+++ utils.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+import os
+
+def get_engine():
+
+    load_dotenv()
+
+    params = f"Driver={os.environ.get('driver')};Server=tcp:{os.environ.get('server')},1433;Database={os.environ.get('database')};Uid={os.environ.get('username')};Pwd={os.environ.get('password')};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'"
+    conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+    engine = create_engine(conn_str,echo=True)
+
+    print('connection is ok')
+    return engine
